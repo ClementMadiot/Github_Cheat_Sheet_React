0fe5952e6f058abe35f7241bef7a4b9a19817dd1
ClementMadiot <clementmadiot09@gmail.com>
2024-01-04 08:29:20 +0100

    add sass

--- cheet_sheet_github/src/components/Header.js
+++ cheet_sheet_github/src/components/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <header>
     <img src={Logo} alt="Icon Github" />
-        <div>
-        <h1>GitHub heat Sheet</h1>
+        <div className='title-home'>
+        <h1>GitHub Cheat Sheet</h1>
         <h2>Versionner son travail</h2>
         </div>
       </header>
