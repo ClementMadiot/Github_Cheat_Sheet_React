402baba4a3ec64799c22de2ac161eb70555667cf
ClementMadiot <clementmadiot09@gmail.com>
2024-01-05 09:56:03 +0100

    manage part CommitOwn + left-part

--- cheet_sheet_github/src/components/CommitOwn.js
+++ cheet_sheet_github/src/components/CommitOwn.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import imgCatGit from "../images/cat-github.png"
+import React from 'react'
+import imgCatGit from '../images/cat-github.png'
 
 const CommitOwnP = () => {
   return (
-    <article className='commit-project'>
-    <aside className="black-box">
-      <div className="title-p-box">
-        <h3>Commit son projet</h3>
-        <div className="head-bar"></div>
-
+    <article className="commit-project">
+      <aside className="black-box">
+        <div className="title-p-box">
+          <h3>Commit son projet sur GitHub</h3>
+          <div className="head-bar"></div>
           <ul>
             <li>
-            <p><strong>git add .</strong></p>
-            <p><strong>git commit -m</strong>"message"</p>
-            <p><strong>git push -u origin main</strong></p>
+              <p>
+                <strong>git add .</strong>
+              </p>
+            </li>
+            <li>
+              <p>
+                <strong>git commit -m</strong>"message"
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <strong>git push -u origin main</strong>
+              </p>
             </li>
           </ul>
-
-      </div>
-    </aside>
-        <aside className="img-git">
-          <img src={imgCatGit} alt="logo animal de github" />
-        </aside>
-
+        </div>
+      </aside>
+      <aside className="img-git">
+        <img src={imgCatGit} alt="logo animal de github" />
+      </aside>
     </article>
-  );
-};
+  )
+}
 
-export default CommitOwnP;
+export default CommitOwnP
