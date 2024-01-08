e7d387ca31415a982fe8918beada95ce10055cdb
ClementMadiot <clementmadiot09@gmail.com>
2024-01-08 17:15:32 +0100

    add button + useState

--- cheet_sheet_github/src/components/Header.js
+++ cheet_sheet_github/src/components/Header.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react'
 import Logo from '../images/logo-github.png'
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const handleClick = event => {
+    setIsButtonClicked(!isButtonClicked)
+    event.currentTarget.classList.toggle(
+      'left','right'
+    )
+  }
+
   
   const OwnWork = 'Versionner son travail'
-  // const TeamWork = 'Travailler en travail'
+  const TeamWork = 'Travailler en travail'
   return (
     <header>
-    <img src={Logo} alt="Icon Github" />
-        <div className='title-home'>
+      <img src={Logo} alt="Icon Github" />
+      <div className="title-home">
         <h1>GitHub Cheat Sheet</h1>
-        <h2>{OwnWork}</h2>
-        {/* <h2>{TeamWork}</h2> */}
-        </div>
-      </header>
-  );
-};
+        <div className="Link-Title">
+          <h2 >{isButtonClicked ? TeamWork : OwnWork }</h2>
+          {/* <h2>{TeamWork}</h2> */}
+          <Link className="Link-btn" to={`/`}>
+            <div className={`arrow ${isButtonClicked ? 'left' : 'right'}`} onClick={handleClick}></div>
+          </Link>
 
-export default Header;
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
